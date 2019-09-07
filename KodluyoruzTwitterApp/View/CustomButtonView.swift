7554e3ff5056ded110b6e968a30a6d66ec68b8c0
kerimcaglar <kerim.caglarr@gmail.com>
2019-09-07 01:35:08 -0700

    added custombuttonview with IBinspectable

---
+++ KodluyoruzTwitterApp/View/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  KodluyoruzTwitterApp
+//
+//  Created by XCODE on 7.09.2019.
+//  Copyright © 2019 kerimcaglar. All rights reserved.
+//
+
+import UIKit
+
+class UIButtonView: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+}
